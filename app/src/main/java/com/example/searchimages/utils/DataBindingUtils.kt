5a97313cfaf3423d803495d88452746c8d17a3ee
Arjun <arjunjalandhari@gmail.com>
2021-09-25 19:59:19 +0530

    - Loading functionality added

--- app/src/main/java/com/example/searchimages/utils/DataBindingUtils.kt
+++ app/src/main/java/com/example/searchimages/utils/DataBindingUtils.kt
@@ -1,5 +1,6 @@
 package com.example.searchimages.utils
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -18,4 +19,9 @@ fun loadTags(chipGroup: ChipGroup, tags: List<String>) {
     tags.forEach {
         chipGroup.addView(Chip(context).apply { text = it })
     }
+}
+
+@BindingAdapter("app:visibleOrGone")
+fun visibleOrGone(view: View, visible: Boolean) {
+    view.visibility = if (visible) View.VISIBLE else View.GONE
 }
